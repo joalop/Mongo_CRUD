cc2f8f9659844593953f7c1f0dc408ed82131399
Joan <joan1998bocairent@gmail.com>
2023-02-20 12:23:18 +0100

    Proyect Succesfull Insertions Documents  and styles added

--- controllers/books_controllers.js
+++ controllers/books_controllers.js
@@ -3,23 +3,71 @@ const mongoConn = require('../database/mongoConn');
 let data, info;
 
 const searchBooks = (req, res, next) => {
-// Ver
-const mostrar = async () => {
-    const books = await  mongoConn.find()
-    return books
+
+    // find() = select Mysql
+    const mostrar = async () => {
+        const books = await  mongoConn.find()
+        return books
+    }
+    let ver = mostrar();
+
+    setTimeout( () => {
+        data = ver
+
+        ver.then(
+
+            (data) => {
+                /*console.log(data, typeof(data));*/
+                info = data
+                res.render('books',{ info})
+            }).catch(
+                (err) => {
+                    console.log(err);
+                    res.redirect('/', { })
+            });
+
+        
+    }, 1000)
+};
+
+// save() = insert Mysql
+const insertBooks = (req, res, next) => {
+    
+    console.log( req.body )
+
+    const crear = async () => {
+        const book = new mongoConn({
+            title: String( req.body.title ),
+            author: String( req.body.author ),
+            image: String( req.body.image ),
+            description: String( req.body.description )
+        })
+        
+        const result = await book.save()
+        console.log(result)
+    };
+    
+    crear();
+
+    setTimeout( ( ) => {
+        res.redirect('/books')
+    },1000)
 }
 
-let ver = mostrar();
+// updateOne ( Uno ) = updated mysql
 
-setTimeout( () => {
-    data = ver
+const updatesBooks = (req, res, next) => {
+    console-log( req.query)
+    const  actualizar = async (id) => {
+        const libro = await BooksModel.updateOne({_id:id},
+            {
+                $set:{
+                    title: 'ESDLA',
+                    author: 'Peter Jackson 2'
+                }
+            })
+            console.log('Actualizar Pasdo')
+    } 
+}
 
-    ver.then(
-        (data) => { /*console.log(data, typeof(data));*/ info = data}).catch(
-            (err) => { console.log(err);
-        });
-
-    res.render('books',{ info})
-}, 500)};
-
-module.exports = { searchBooks }
+module.exports = { searchBooks, insertBooks, updatesBooks }
--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router();
 
-const { searchBooks } = require('../controllers/books_controllers')
+const { searchBooks, insertBooks, updatesBooks } = require('../controllers/books_controllers')
 
 router.get('/', searchBooks );
 
 router.get('/new', (req, res, next) => {
     res.render('new', {})
+    
 });
+
+router.post('/new', insertBooks);
+
+router.get('/Update', updatesBooks)
 
 module.exports = router
